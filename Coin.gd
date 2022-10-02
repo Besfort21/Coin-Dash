f6f41bcbea52e53782b1fb05695d46c83a8b9e36
Besfort21 <besfort-avdyli1@hotmail.com>
2022-10-02 15:00:59 +0200

    Initial commit

---
+++ Coin.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var screensize = Vector2()
+
+func pickup():
+	queue_free()
+
+
+
+func _ready():
+	pass 
+
+
+
+func _process(delta):
+	pass
---
+++ HUD.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+
+signal start_game
+signal new_game
+
+func update_score(value):
+	$MarginContainer/ScoreLabel.text = str(value)
+
+func update_timer(value):
+	$MarginContainer/TimeLabel.text = str(value)
+
+func show_message(text):
+	$MessageLabel.text  = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer,"timeout")
+	$StartButton.show()
+	$MessageLabel.text = "Coin Dash!"
+	$MessageLabel.show()
+
+
+func _ready():
+	pass 
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	$MessageLabel.hide()
+	emit_signal("new_game")
